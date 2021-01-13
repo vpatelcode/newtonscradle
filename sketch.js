88e28f0a7bb5b5b7de3df25af57aafca20339bff
vpatelcode <72821561+vpatelcode@users.noreply.github.com>
2021-01-13 15:12:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Mouse = Matter.Mouse;
+
+const Constraint = Matter.Constraint;
+
+const MouseConstraint = Matter.MouseConstraint;
+
+var world, engine, canvas;
+var sling1, sling2, sling3, sling4, sling5;
+var mConstraint;
+var pendulum1, pendulum2, pendulum3, pendulum4, pendulum5;
+
+function setup()
+ {
+  canvas = createCanvas(windowWidth/2, windowHeight / 1.5);
+  engine = Engine.create();
+  world =engine.world;
+
+  
+  let canvasmouse = Mouse.create(canvas.elt);
+  canvasmouse.pixelRatio = pixelDensity();
+  let options = {
+    mouse: canvasmouse
+  };
+
+  mConstraint = MouseConstraint.create (engine,options);
+  
+  World.add(world, mConstraint);
+
+  pendulum1 = new Pendulum(340, 450, 100);
+  pendulum2 = new Pendulum(400, 450, 100);
+  pendulum3 = new Pendulum(460, 450, 100);
+  pendulum4 = new Pendulum(520, 450, 100);
+  pendulum5 = new Pendulum(580, 450, 100);
+
+  sling1 = new Sling(pendulum1.body, { x: 340, y: 200 });
+  sling2 = new Sling(pendulum2.body, { x: 400, y: 200 });
+  sling3 = new Sling(pendulum3.body, { x: 460, y: 200 });
+  sling4 = new Sling(pendulum4.body,{ x: 520, y: 200 });
+  sling5 = new Sling(pendulum5.body, { x: 580, y: 200 });
+}
+
+function draw() {
+    
+  background(0);
+  Engine.update(engine);
+
+  pendulum1.display();
+  pendulum2.display();
+  pendulum4.display();
+  pendulum3.display();
+  pendulum5.display();
+
+  sling1.display();
+  sling2.display();
+  sling3.display();
+  sling4.display();
+  sling5.display();
+}
+
+function mouseDragged() 
+{
+  Matter.Body.setPosition(pendulum1.body, { x: mouseX, y: mouseY });
+}
